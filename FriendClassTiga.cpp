89f0d55ea31560bc303f265176ce3c3b1eb6cf2f
Lailansyahda <syahdaazalia123@gmail.com>
2025-05-05 20:27:49 +0700

    last commit

--- FriendClassTiga.cpp
+++ FriendClassTiga.cpp
@@ -11,9 +11,21 @@ class manusia
 class pelajar
 {
     private :
-    int nilai :
+    int nilai;
 
     public :
     pelajar() { nilai = 100; }
     friend void manusia::showNilaiPelajar(pelajar &x);
 };
+
+void manusia::showNilaiPelajar(pelajar &x)
+{
+    cout << x.nilai;
+}
+int main()
+{
+    manusia budi;
+    pelajar pbudi;
+    budi.showNilaiPelajar(pbudi);
+    return 0;
+}
